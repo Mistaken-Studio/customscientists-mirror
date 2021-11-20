cc6bfa5854387af6344b883574b8da774498ea5f
barwa <14200789+barwa100@users.noreply.github.com>
2021-11-20 19:18:22 +0100

    Add project files.

---
+++ CustomScientists/Classes/DeputyFacalityManager.cs
@@ -0,0 +1,55 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="DeputyFacalityManager.cs" company="Mistaken">
+// Copyright (c) Mistaken. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Collections.Generic;
+using Exiled.API.Features;
+using Mistaken.API.CustomRoles;
+
+namespace Mistaken.CustomScientists.Classes
+{
+    /// <inheritdoc/>
+    public class DeputyFacalityManager : Mistaken.API.CustomRoles.MistakenCustomRole
+    {
+        /// <inheritdoc/>
+        public override MistakenCustomRoles CustomRole => MistakenCustomRoles.DEPUTY_FACILITY_MANAGER;
+
+        /// <inheritdoc/>
+        public override RoleType Role { get; set; } = RoleType.Scientist;
+
+        /// <inheritdoc/>
+        public override int MaxHealth { get; set; } = 100;
+
+        /// <inheritdoc/>
+        public override string Name { get; set; } = "Zastępca Dyrektora Placówki";
+
+        /// <inheritdoc/>
+        public override string Description { get; set; } = "Twoim zadaniem jest pomoc ochronie w odeskortowaniu <color=yellow>naukowców</color><br>Nie możesz uciec przed dekontaminacją LCZ";
+
+        /// <inheritdoc/>
+        public override void AddRole(Player player)
+        {
+            base.AddRole(player);
+        }
+
+        /// <inheritdoc/>
+        protected override bool KeepInventoryOnSpawn { get; set; } = false;
+
+        /// <inheritdoc/>
+        protected override bool KeepRoleOnDeath { get; set; } = false;
+
+        /// <inheritdoc/>
+        protected override bool RemovalKillsPlayer { get; set; } = true;
+
+        /// <inheritdoc/>
+        protected override List<string> Inventory { get; set; } = new List<string>()
+        {
+            ItemType.Adrenaline.ToString(),
+            ItemType.Medkit.ToString(),
+            ItemType.Radio.ToString(),
+            ItemType.ArmorLight.ToString(),
+        };
+    }
+}
---
+++ CustomScientists/Items/DeputyFacalityManagerKeycard.cs
@@ -0,0 +1,86 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="DeputyFacalityManagerKeycard.cs" company="Mistaken">
+// Copyright (c) Mistaken. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.API.Enums;
+using Exiled.API.Features;
+using Exiled.API.Features.Spawn;
+using Exiled.Events.EventArgs;
+using Mistaken.API;
+using Mistaken.API.CustomItems;
+using UnityEngine;
+
+namespace Mistaken.CustomScientists.Items
+{
+    /// <inheritdoc/>
+    public class DeputyFacalityManagerKeycard : MistakenCustomItem
+    {
+        /// <inheritdoc/>
+        public override MistakenCustomItems CustomItem => MistakenCustomItems.DEPUTY_FACILITY_MANAGER_KEYCARD;
+
+        /// <inheritdoc/>
+        public override ItemType Type { get; set; } = ItemType.KeycardFacilityManager;
+
+        /// <inheritdoc/>
+        public override string Name { get; set; } = "Karta Zastępcy Dyrektora Placówki";
+
+        /// <inheritdoc/>
+        public override string Description { get; set; } = "well";
+
+        /// <inheritdoc/>
+        public override string DisplayName { get; set; } = "<color=#bd1a47>karta Zastępcy Dyrektora Placówki</color>";
+
+        /// <inheritdoc/>
+        public override float Weight { get; set; } = 0.5f;
+
+        /// <inheritdoc/>
+        public override SpawnProperties SpawnProperties { get; set; } = new SpawnProperties();
+
+        /// <inheritdoc/>
+        protected override void SubscribeEvents()
+        {
+            base.SubscribeEvents();
+            Exiled.Events.Handlers.Player.InteractingDoor += this.Player_InteractingDoor;
+        }
+
+        /// <inheritdoc/>
+        protected override void UnsubscribeEvents()
+        {
+            base.UnsubscribeEvents();
+            Exiled.Events.Handlers.Player.InteractingDoor += this.Player_InteractingDoor;
+        }
+
+        private void Player_InteractingDoor(InteractingDoorEventArgs ev)
+        {
+            if (MapPlus.IsLCZDecontaminated())
+                return;
+            var type = ev.Door.Type;
+            if (type == DoorType.GateA || type == DoorType.GateB)
+            {
+                foreach (var player in RealPlayers.List.Where(p => p.Id != ev.Player.Id && p.Role == RoleType.Scientist))
+                {
+                    if (Vector3.Distance(player.Position, ev.Player.Position) < 10)
+                    {
+                        ev.IsAllowed = true;
+                        return;
+                    }
+                }
+
+                ev.IsAllowed = false;
+                return;
+            }
+            else if (type == DoorType.NukeSurface || type == DoorType.Scp106Primary || type == DoorType.Scp106Secondary)
+            {
+                ev.IsAllowed = false;
+                return;
+            }
+        }
+    }
+}
