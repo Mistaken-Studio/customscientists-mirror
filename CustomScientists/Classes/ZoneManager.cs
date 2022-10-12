3abdc47e478c687b0c39f25c214b36e1161ddf54
Bartosz Barwiński <barwinski.b@protonmail.com>
2022-10-12 21:02:18 +0200

    Move hierarchy refresh further

--- CustomScientists/Classes/ZoneManager.cs
+++ CustomScientists/Classes/ZoneManager.cs
@@ -84,11 +84,11 @@ namespace Mistaken.CustomScientists.Classes
         protected override void RoleAdded(Player player)
         {
             base.RoleAdded(player);
-            if(PluginHandler.CustomHierarchyIntegrationEnabled)
-                CustomHierarchyIntegration.UpdateHierarchy(player);
             MEC.Timing.CallDelayed(1f, () =>
             {
                 player.Position = API.Utilities.Room.Get(Room.List.First(x => x.Type == RoomType.LczClassDSpawn)).Neighbors[0].ExiledRoom.Position + (Vector3.up * 2f);
+                if(PluginHandler.CustomHierarchyIntegrationEnabled)
+                    CustomHierarchyIntegration.UpdateHierarchy(player);
             });
         }
 
