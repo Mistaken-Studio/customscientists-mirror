9208f5d8aeccd48430caea7705843728d4999976
Xname <xnameserver@o2.pl>
2022-10-23 01:13:49 +0200

    Remove debug comments

--- CustomScientists/CustomHierarchyIntegration.cs
+++ CustomScientists/CustomHierarchyIntegration.cs
@@ -30,11 +30,6 @@ namespace Mistaken.CustomScientists
                     var p1z = Classes.ZoneManager.Instance.Check(p1);
                     var p2z = Classes.ZoneManager.Instance.Check(p2);
 
-                    // Log.Debug($"Player 1 is Deputy Facality Manager: {p1c}", PluginHandler.Instance.Config.VerbouseOutput);
-                    // Log.Debug($"Player 2 is Deputy Facality Manager: {p2c}", PluginHandler.Instance.Config.VerbouseOutput);
-                    // Log.Debug($"Player 1 is Zone Manager: {p1z}", PluginHandler.Instance.Config.VerbouseOutput);
-                    // Log.Debug($"Player 2 is Zone Manager: {p2z}", PluginHandler.Instance.Config.VerbouseOutput);
-
                     if ((p1c && p2c) || (p1z && p2z))
                         return CompareResult.SAME_RANK;
                     else if (p1c)
